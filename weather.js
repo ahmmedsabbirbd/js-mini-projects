3bb199920f68a62dd069499c71099831a8a58ea7
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-06-21 13:07:19 +0600

    complete project

--- weather.js
+++ weather.js
@@ -1,15 +1,15 @@
 const Weather = {
   getWeatherData:async (apiUrl, location) => {
     try {
-      const response = await fetch(apiUrl + location);
-      const data = await response.json();
-      return data;
+      const response = await fetch(apiUrl + location)
+      const data = await response.json()
+      return data
     } catch (error) {
-      throw new Error('Failed to fetch weather data');
+      throw new Error('Failed to fetch weather data')
     }
   },
   displayWeatherInfo (weather) {
-    const weatherInfo = document.getElementById('weatherInfo');
+    const weatherInfo = document.getElementById('weatherInfo')
     if(!weather.error) {
       weatherInfo.innerHTML = `
         <h4>Weather Information</h4>
@@ -17,19 +17,19 @@ const Weather = {
         <p>Temperature: ${weather.current.temp_c}°C</p>
         <p>Condition: ${weather.current.condition.text}</p>
         <p>Humidity: ${weather.current.humidity}%</p>
-      `;
+      `
     } else {
       weatherInfo.innerHTML = `
         <p>${weather.error.message}</p>
-      `;
+      `
     }
   },
   handleSubmit:async (apiUrl) => {
-    const locationInput = document.getElementById('locationInput');
-    const location = locationInput.value.trim();
+    const locationInput = document.getElementById('locationInput')
+    const location = locationInput.value.trim()
     
     if (location !== '') {
-      const weatherData = await Weather.getWeatherData(apiUrl, location);
+      const weatherData = await Weather.getWeatherData(apiUrl, location)
       Weather.displayWeatherInfo(weatherData)
     }
   }
